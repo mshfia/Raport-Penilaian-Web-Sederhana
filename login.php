d0e2b72ebce6c136dcdd139018749e1463d4fa9f
Mahrus Shofi <mshfia31@gmail.com>
2025-01-21 19:03:11 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,19 @@
+/*
+  | 
+  | Source Code Aplikasi Raport Penilaian dengan PHP & MYSQL
+  | 
+  | @package   : Raport-php
+  | @file	     : db.php ( untuk mengatur koneksi php ke database mysql )
+  | @author    : Mahrus Mehmed
+  | @-mail     : mehmedmahrus@gmail.com
+  | 
+  | Jangan Sering Bengong Ya dek Ya...
+  | Ingatt. suliiiit...
+  |
+  | keterangan : untuk login aplikasi dengan username : mahrus dan password : 12345
+  | 
+ */
+
 <?php
 session_start();
 // Koneksi ke database
